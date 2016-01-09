9134f78f6e1a6745dbf28be45b505670e8c0dc95
Scientist94 <shivam.bhattacharjee94@gmail.com>
2016-01-09 17:39:09 +0530

    contact test updated with test_field

--- contact/tests.py
+++ contact/tests.py
@@ -1,5 +1,6 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
 from .models import ContactForm
+from .forms import ContactView
 from datetime import datetime, timedelta
 
 
@@ -23,5 +24,11 @@ class UserModelTest(TestCase):
 	def test_order(self):
 		contacts = ContactForm.objects.all()
 		self.assertEquals(self.firstUser, contacts[0])
+
+class ContactViewTests(SimpleTestCase):
+
+	def test_displayed_fields(self):
+		expected_fields = ['name', 'email', 'topic', 'message']
+		self.assertEquals(ContactView.Meta.fields, expected_fields)
 
 		
